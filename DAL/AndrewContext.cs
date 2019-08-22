6421a714b0813b3727c6f6f2b5ec2afcd72fe0c7
Andrew Critchley <andrew.critchley@convatec.com>
2019-08-22 15:23:24 +0100

    first commit

---
+++ DAL/AndrewContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using EFCF.DAL.Models;
+
+namespace EFCF.DAL
+{
+    public class AndrewContext : DbContext
+    {
+        public AndrewContext() : base("name=AndrewContext")
+    {
+    }
+        public IDbSet<Customer> Customers { get; set; }
+        public IDbSet<Order> Orders { get; set; }
+    }
+}
---
+++ DAL/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿namespace EFCF.DAL.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+
+        public string Surname { get; set; }
+
+        public string AddressLine1 { get; set; }
+    }
+}
